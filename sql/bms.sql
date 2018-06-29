a50e79c9ffe7d906fe86995333b5305f8f7f99af
ujjal sannyal <ujjal.sannyal@SPI.COM>
2018-06-29 06:19:02 -0700

    add new sql file

--- sql/bms.sql
+++ sql/bms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2018 at 03:23 PM
+-- Generation Time: Jun 29, 2018 at 03:17 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -30,30 +30,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `booking_cart` (
   `id` int(11) NOT NULL,
-  `cart_id` varchar(255) NOT NULL,
-  `order_id` varchar(255) NOT NULL,
-  `slot_id` int(10) NOT NULL,
+  `cart_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `slot_id` int(11) NOT NULL,
+  `service_cart_id` int(11) NOT NULL,
   `price` int(10) NOT NULL,
-  `user_id` int(10) NOT NULL,
-  `studio_id` int(10) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `studio_id` int(11) NOT NULL,
   `booking_time` varchar(250) NOT NULL,
   `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `booking_cart`
---
-
-INSERT INTO `booking_cart` (`id`, `cart_id`, `order_id`, `slot_id`, `price`, `user_id`, `studio_id`, `booking_time`, `created_at`, `updated_at`) VALUES
-(28, '1529576148120', '', 1, 2000, 2, 1, '8pm', '2018-06-21 03:15:48.165688', '2018-06-21 03:15:48.165688'),
-(29, '1529576148120', '', 2, 2500, 2, 1, '9pm', '2018-06-21 03:15:48.165688', '2018-06-21 03:15:48.165688'),
-(30, '1529576148120', '', 1, 2000, 2, 1, '8pm', '2018-06-22 04:32:17.639894', '2018-06-22 04:32:17.639894'),
-(31, '1529576148120', '', 2, 2500, 2, 1, '9pm', '2018-06-22 04:32:17.639894', '2018-06-22 04:32:17.639894'),
-(32, '1529576148120', '', 1, 2000, 2, 1, '8pm', '2018-06-22 04:34:31.015173', '2018-06-22 04:34:31.015173'),
-(33, '1529576148120', '', 2, 2500, 2, 1, '9pm', '2018-06-22 04:34:31.015173', '2018-06-22 04:34:31.015173'),
-(34, '1529576148120', '', 1, 2000, 2, 1, '8pm', '2018-06-22 04:35:43.200242', '2018-06-22 04:35:43.200242'),
-(35, '1529576148120', '', 2, 2500, 2, 1, '9pm', '2018-06-22 04:35:43.200242', '2018-06-22 04:35:43.200242');
 
 -- --------------------------------------------------------
 
@@ -63,27 +50,17 @@ INSERT INTO `booking_cart` (`id`, `cart_id`, `order_id`, `slot_id`, `price`, `us
 
 CREATE TABLE `booking_service_cart` (
   `id` int(11) NOT NULL,
-  `cart_id` varchar(255) NOT NULL,
-  `order_id` varchar(255) NOT NULL,
-  `service_id` varchar(250) NOT NULL,
+  `cart_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
   `price` varchar(250) NOT NULL,
   `service_count` int(10) NOT NULL,
-  `required_slots` varchar(250) NOT NULL,
-  `user_id` varchar(250) NOT NULL,
-  `studio_id` int(10) NOT NULL,
+  `required_slots` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `studio_id` int(11) NOT NULL,
   `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `booking_service_cart`
---
-
-INSERT INTO `booking_service_cart` (`id`, `cart_id`, `order_id`, `service_id`, `price`, `service_count`, `required_slots`, `user_id`, `studio_id`, `created_at`, `updated_at`) VALUES
-(48, '1529576148120', '', '1', '3000', 3, '1', '2', 1, '2018-06-22 05:15:36.368162', '2018-06-22 05:15:36.368162'),
-(49, '1529576148120', '', '2', '2000', 1, '0', '2', 1, '2018-06-22 05:15:36.368162', '2018-06-22 05:15:36.368162'),
-(50, '1529576148120', '', '1', '3000', 3, '1', '2', 1, '2018-06-22 05:17:27.928893', '2018-06-22 05:17:27.928893'),
-(51, '1529576148120', '', '2', '2000', 1, '0', '2', 1, '2018-06-22 05:17:27.928893', '2018-06-22 05:17:27.928893');
 
 -- --------------------------------------------------------
 
@@ -93,13 +70,13 @@ INSERT INTO `booking_service_cart` (`id`, `cart_id`, `order_id`, `service_id`, `
 
 CREATE TABLE `cart` (
   `id` int(11) NOT NULL,
-  `cartid` varchar(250) NOT NULL,
-  `orderid` varchar(250) NOT NULL,
-  `total` int(10) NOT NULL,
-  `status` varchar(250) NOT NULL,
-  `user_id` varchar(250) NOT NULL,
-  `recorddate` datetime(6) NOT NULL,
-  `luta` datetime(6) NOT NULL
+  `cart_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -121,38 +98,16 @@ CREATE TABLE `comments` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `holiday_list_master`
---
-
-CREATE TABLE `holiday_list_master` (
-  `id` int(11) NOT NULL,
-  `studio_id` int(10) NOT NULL,
-  `date` date NOT NULL,
-  `remarks` varchar(250) NOT NULL,
-  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `holiday_list_master`
---
-
-INSERT INTO `holiday_list_master` (`id`, `studio_id`, `date`, `remarks`, `created_at`, `updated_at`) VALUES
-(1, 1, '2018-06-24', 'closed for sunday', '2018-06-12 22:30:15.000000', '2018-06-12 22:30:15.000000'),
-(2, 1, '2018-06-30', '', '2018-06-12 22:30:15.000000', '2018-06-12 22:30:15.000000');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `orders`
 --
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
-  `cart_id` varchar(250) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cart_id` int(11) NOT NULL,
   `status` varchar(250) NOT NULL,
   `total` int(10) NOT NULL,
-  `user_id` varchar(250) NOT NULL,
   `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -266,7 +221,7 @@ CREATE TABLE `studio_service` (
   `id` int(11) NOT NULL,
   `service_id` int(11) NOT NULL,
   `slots_required` varchar(250) NOT NULL,
-  `studio_id` varchar(250) NOT NULL,
+  `studio_id` int(11) NOT NULL,
   `price` int(10) NOT NULL,
   `created_at` datetime(6) NOT NULL,
   `updated_at` datetime(6) NOT NULL
@@ -277,8 +232,8 @@ CREATE TABLE `studio_service` (
 --
 
 INSERT INTO `studio_service` (`id`, `service_id`, `slots_required`, `studio_id`, `price`, `created_at`, `updated_at`) VALUES
-(1, 1, '1', '1', 3000, '2018-06-12 00:00:00.000000', '2018-06-12 06:25:10.000000'),
-(2, 3, '0', '1', 2000, '2018-06-12 00:00:00.000000', '2018-06-12 06:25:10.000000');
+(1, 1, '1', 1, 3000, '2018-06-12 00:00:00.000000', '2018-06-12 06:25:10.000000'),
+(2, 3, '0', 1, 2000, '2018-06-12 00:00:00.000000', '2018-06-12 06:25:10.000000');
 
 -- --------------------------------------------------------
 
@@ -289,6 +244,7 @@ INSERT INTO `studio_service` (`id`, `service_id`, `slots_required`, `studio_id`,
 CREATE TABLE `studio_timeslots` (
   `id` int(11) NOT NULL,
   `studio_id` int(10) NOT NULL,
+  `studio_service_id` int(11) NOT NULL,
   `8am` varchar(250) NOT NULL,
   `9am` varchar(250) NOT NULL,
   `10am` varchar(250) NOT NULL,
@@ -313,20 +269,10 @@ CREATE TABLE `studio_timeslots` (
   `5am` varchar(250) NOT NULL,
   `6am` varchar(250) NOT NULL,
   `7am` varchar(250) NOT NULL,
-  `date` date NOT NULL,
+  `day` date NOT NULL,
   `created_at` datetime(6) DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `studio_timeslots`
---
-
-INSERT INTO `studio_timeslots` (`id`, `studio_id`, `8am`, `9am`, `10am`, `11am`, `12pm`, `1pm`, `2pm`, `3pm`, `4pm`, `5pm`, `6pm`, `7pm`, `8pm`, `9pm`, `10pm`, `11pm`, `12am`, `1am`, `2am`, `3am`, `4am`, `5am`, `6am`, `7am`, `date`, `created_at`, `updated_at`) VALUES
-(1, 1, '1000', '1000', '1000', '1000', '1000', '1000', '1000', '0', '1000', '1000', '1000', '0', '2000', '2500', '3000', '0', '1000', '0', '1000', '4000', '4000', '0', '4000', '4000', '2018-06-26', '2018-06-12 22:14:01.000000', '2018-06-12 22:14:01.000000'),
-(2, 1, '1000', '0', '0', '1000', '0', '0', '1000', '0', '0', '0', '1000', '0', '2000', '2500', '3000', '0', '0', '1000', '0', '4000', '4000', '0', '4000', '4000', '2018-06-27', '2018-06-12 22:14:28.000000', '2018-06-12 22:14:28.000000'),
-(3, 1, '0', '1000', '0', '0', '1000', '0', '1000', '0', '1000', '0', '1000', '0', '2000', '2500', '3000', '0', '0', '1000', '0', '4000', '4000', '0', '4000', '4000', '2018-06-28', '2018-06-12 22:14:56.000000', '2018-06-12 22:14:56.000000'),
-(4, 1, '0', '1000', '0', '0', '1000', '0', '1000', '0', '0', '1000', '0', '0', '2000', '2500', '3000', '0', '0', '1000', '0', '4000', '4000', '0', '4000', '4000', '2018-06-29', '2018-06-12 22:14:56.000000', '2018-06-12 22:14:56.000000');
 
 -- --------------------------------------------------------
 
@@ -365,38 +311,49 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `address`, `zip`, `
 -- Indexes for table `booking_cart`
 --
 ALTER TABLE `booking_cart`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `studio_id` (`studio_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `slot_id` (`slot_id`),
+  ADD KEY `cart_id` (`cart_id`),
+  ADD KEY `service_cart_id` (`service_cart_id`);
 
 --
 -- Indexes for table `booking_service_cart`
 --
 ALTER TABLE `booking_service_cart`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `studio_id` (`studio_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `service_id` (`service_id`),
+  ADD KEY `cart_id` (`cart_id`),
+  ADD KEY `booking_service_cart_ibfk_3` (`order_id`);
 
 --
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `cart_id` (`cart_id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `holiday_list_master`
---
-ALTER TABLE `holiday_list_master`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `studio_id` (`studio_id`);
+  ADD KEY `studio_id` (`studio_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `order_id` (`order_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `cart_id` (`cart_id`);
 
 --
 -- Indexes for table `service_master`
@@ -408,26 +365,31 @@ ALTER TABLE `service_master`
 -- Indexes for table `studio`
 --
 ALTER TABLE `studio`
-  ADD PRIMARY KEY (`studio_id`);
+  ADD PRIMARY KEY (`studio_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `studio_ratings`
 --
 ALTER TABLE `studio_ratings`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `studio_ratings_ibfk_1` (`studio_id`);
 
 --
 -- Indexes for table `studio_service`
 --
 ALTER TABLE `studio_service`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `service_id` (`service_id`);
+  ADD KEY `service_id` (`service_id`),
+  ADD KEY `studio_id` (`studio_id`);
 
 --
 -- Indexes for table `studio_timeslots`
 --
 ALTER TABLE `studio_timeslots`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `studio_service_id` (`studio_service_id`),
+  ADD KEY `Studio Id` (`studio_id`);
 
 --
 -- Indexes for table `users`
@@ -443,13 +405,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `booking_cart`
 --
 ALTER TABLE `booking_cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `booking_service_cart`
 --
 ALTER TABLE `booking_service_cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `cart`
@@ -462,12 +424,6 @@ ALTER TABLE `cart`
 --
 ALTER TABLE `comments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `holiday_list_master`
---
-ALTER TABLE `holiday_list_master`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -503,7 +459,7 @@ ALTER TABLE `studio_service`
 -- AUTO_INCREMENT for table `studio_timeslots`
 --
 ALTER TABLE `studio_timeslots`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -516,16 +472,71 @@ ALTER TABLE `users`
 --
 
 --
--- Constraints for table `holiday_list_master`
+-- Constraints for table `booking_cart`
 --
-ALTER TABLE `holiday_list_master`
-  ADD CONSTRAINT `holiday_list_master_ibfk_1` FOREIGN KEY (`studio_id`) REFERENCES `studio` (`studio_id`);
+ALTER TABLE `booking_cart`
+  ADD CONSTRAINT `booking_cart_ibfk_1` FOREIGN KEY (`studio_id`) REFERENCES `studio` (`studio_id`),
+  ADD CONSTRAINT `booking_cart_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `booking_cart_ibfk_3` FOREIGN KEY (`slot_id`) REFERENCES `studio_timeslots` (`id`),
+  ADD CONSTRAINT `booking_cart_ibfk_4` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`),
+  ADD CONSTRAINT `booking_cart_ibfk_5` FOREIGN KEY (`service_cart_id`) REFERENCES `booking_service_cart` (`id`);
+
+--
+-- Constraints for table `booking_service_cart`
+--
+ALTER TABLE `booking_service_cart`
+  ADD CONSTRAINT `booking_service_cart_ibfk_1` FOREIGN KEY (`studio_id`) REFERENCES `studio` (`studio_id`),
+  ADD CONSTRAINT `booking_service_cart_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `booking_service_cart_ibfk_3` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  ADD CONSTRAINT `booking_service_cart_ibfk_4` FOREIGN KEY (`service_id`) REFERENCES `studio_service` (`id`),
+  ADD CONSTRAINT `booking_service_cart_ibfk_5` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`);
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`studio_id`) REFERENCES `studio` (`studio_id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`);
+
+--
+-- Constraints for table `studio`
+--
+ALTER TABLE `studio`
+  ADD CONSTRAINT `studio_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `studio_ratings`
+--
+ALTER TABLE `studio_ratings`
+  ADD CONSTRAINT `studio_ratings_ibfk_1` FOREIGN KEY (`studio_id`) REFERENCES `studio` (`studio_id`);
 
 --
 -- Constraints for table `studio_service`
 --
 ALTER TABLE `studio_service`
-  ADD CONSTRAINT `studio_service_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `service_master` (`id`);
+  ADD CONSTRAINT `studio_service_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `service_master` (`id`),
+  ADD CONSTRAINT `studio_service_ibfk_2` FOREIGN KEY (`studio_id`) REFERENCES `studio` (`studio_id`);
+
+--
+-- Constraints for table `studio_timeslots`
+--
+ALTER TABLE `studio_timeslots`
+  ADD CONSTRAINT `Studio Id` FOREIGN KEY (`studio_id`) REFERENCES `studio` (`studio_id`),
+  ADD CONSTRAINT `studio_timeslots_ibfk_1` FOREIGN KEY (`studio_service_id`) REFERENCES `studio_service` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
